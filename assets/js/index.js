40d801ba53752ff897c40a01ede0ae022d14b43d
Francesco De Benedittis <francinformation@gmail.com>
2020-01-06 18:25:20 +0100

    Added Dark Theme

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+function isLsSupported() {
+    try {
+        localStorage.setItem("check", "check");
+        localStorage.removeItem("check");
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+
+function switchTheme() {
+    if(isLsSupported()) {
+        var currentTheme = localStorage.currentTheme;
+        if(currentTheme === "default") {
+            localStorage.currentTheme = "dark";
+        }
+        else if(currentTheme === "dark") {
+            localStorage.currentTheme = "default";
+        }
+        location.reload();
+    }
+}
+
+window.onload = function() {
+    if(isLsSupported()) {
+        var currentTheme = localStorage.currentTheme;
+        if(!currentTheme) {
+            localStorage.currentTheme = "default";
+            currentTheme = "default";
+        }
+        if(currentTheme === "default") {
+            document.getElementById("default-theme").disabled = false;
+            document.getElementById("dark-theme").disabled = true;
+        }
+        else if(currentTheme === "dark") {
+            document.getElementById("dark-theme").disabled = false;
+            document.getElementById("default-theme").disabled = true;
+        }
+    }
+    else {
+        document.getElementById("default-theme").disabled = false;
+        document.getElementById("dark-theme").disabled = true;
+        document.getElementById("footer-switchtheme").innerHTML = "";
+    }
+    // HitCounter Updater
+        var req = new XMLHttpRequest();
+        req.open("GET", "https://hitcounter.pythonanywhere.com/count?url=francescodb.me", true);
+        req.send(null);
+};
